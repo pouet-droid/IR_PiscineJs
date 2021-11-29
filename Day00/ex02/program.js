071b505248d5996ba2bc8b118fa9fa0b4620abb1
pouet-droid <72503936+pouet-droid@users.noreply.github.com>
2021-11-29 14:44:37 +0100

    ex03

---
+++ Day00/ex02/program.js
@@ -0,0 +1,16 @@
+function threeLargestNumbers(array){
+    const threeLargestNumbers = [array[0], array[1], array[2]];
+    for (let i = 3; i < array.length; i++) {
+        let keyLowest = 0;
+        for (let j = 1; j < threeLargestNumbers.length; j++) {
+            if(threeLargestNumbers[j] < threeLargestNumbers[keyLowest]){
+                keyLowest = j; 
+            }
+        }
+        if(threeLargestNumbers[keyLowest] < array[i]){
+            threeLargestNumbers[keyLowest] = array[i]; 
+        }
+    }
+    return threeLargestNumbers.sort((a, b) => a - b);    
+} 
+exports.threeLargestNumbers =  threeLargestNumbers;
