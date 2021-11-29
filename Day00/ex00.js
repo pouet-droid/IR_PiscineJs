bd2330a84515f7625b4067d4fad4920381c60bbf
pouet-droid <72503936+pouet-droid@users.noreply.github.com>
2021-11-29 11:17:22 +0100

    ex00

--- Day00/ex00.js
+++ Day00/ex00.js
@@ -1,13 +1,14 @@
-function somme(array, resultat){
+function twoNumberSum(array, targetSum){
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
             if (j == i) {
                 j++;
             }
-            if (array[i] + array[j] == resultat) {
+            if (array[i] + array[j] == targetSum) {
                 return [array[i], array[j]];
             }            
         }
     }
     return [];
 }
+exports.twoNumberSum =  twoNumberSum;
